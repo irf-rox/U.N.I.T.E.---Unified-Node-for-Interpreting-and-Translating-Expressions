376216cfc4a39774ce321154a37c93a1e41827c5
irf-rox <94896261+irf-rox@users.noreply.github.com>
2023-12-06 20:47:13 +0530

    Add files via upload

---
+++ Code/collect_imgs.py
@@ -0,0 +1,36 @@
+import os
+import cv2
+DATA_DIR = './dataset1'
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+x
+number_of_classes = 1
+dataset_size = 100
+
+cap = cv2.VideoCapture(0)
+for j in range(number_of_classes):
+    if not os.path.exists(os.path.join(DATA_DIR, str(j))):
+        os.makedirs(os.path.join(DATA_DIR, str(j)))
+
+    print('Collecting data for class {}'.format(j))
+
+    done = False
+    while True:
+        ret, frame = cap.read()
+        cv2.putText(frame, 'Press Spacebar when ready', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+                    cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(25) == 32:
+            break
+
+    counter = 0
+    while counter < dataset_size:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        cv2.waitKey(25)
+        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+
+        counter += 1
+
+cap.release()
+cv2.destroyAllWindows()
